d1bac7cc3e781611e4c538c658fc9807236b8f12
Lucas Llano <llano.lucas.04@gmail.com>
2024-01-29 13:37:04 -0300

    current middleware corregido

--- src/middlewares/current.middleware.js
+++ src/middlewares/current.middleware.js
@@ -1,6 +1,8 @@
 export const current = (roles) => {
   return (req, res, next) => {
-    if (req.user.role === roles) return next();
+    let role = req.user.role;
+    if (role === undefined) role = req.user.user.role;
+    if (role === roles) return next();
 
     return res.status(400).send("Not authorized");
   };
